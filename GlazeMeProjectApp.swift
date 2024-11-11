31e6a1b288cb647ec7a4cc8e8556a1a003cd3151
Ryan LaTorre <ryanlatorre26@gmailcom>
2024-11-11 10:31:44 -0600

    Initial commit with Xcode project files

---
+++ GlazeMeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlazeMeProjectApp.swift
+//  GlazeMeProject
+//
+//  Created by Ryan LaTorre on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GlazeMeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
