ee038c08f1b9ba448d0f77ae2e426e375994d4f6
Ryan LaTorre <ryanlatorre26@gmailcom>
2024-11-11 10:35:52 -0600

    TESTING Updated ContentView text

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,3 @@
-//
-//  ContentView.swift
-//  GlazeMeProject
-//
-//  Created by Ryan LaTorre on 11/11/24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
@@ -13,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Hello, Glaze!")
         }
         .padding()
     }
